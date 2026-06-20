03dbcefe25e308bd213ea6103362060ef657c810
Xinyu Yang <Pana.Yang@hotmail.com>
2026-06-20 14:54:57 +0800

    bump: v3.0.7

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! bincode-next = "3.0.6"
+//! bincode-next = "3.0.7"
 //! ```
 //!
 //! ### Basic Encode / Decode
@@ -118,7 +118,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! bincode-next = { version = "3.0.6", features = ["serde"] }
+//! bincode-next = { version = "3.0.7", features = ["serde"] }
 //! serde = { version = "1", features = ["derive"] }
 //! ```
 //!
